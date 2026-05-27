27c565d62c39c6caa5d276a29346f7cafd0c6d83
Long Phan <long.phan@karatnet.com>
2026-05-27 08:25:13 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Task_DirectoryTracker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
