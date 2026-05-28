d08cb8b29eee0a264666c4677a2a0fb716f4a03b
Long Phan <long.phan@karatnet.com>
2026-05-28 09:21:21 +0200

    Extract DI registrations into separate configuration file
    Moved service and app registrations from Program.cs into a dedicated
    DI configuration file to improve readability and separation of concerns.

---
+++ DependencyInjection.cs
@@ -0,0 +1,58 @@
+﻿using Task_DirectoryTracker.Abstractions;
+using Task_DirectoryTracker.Middlewares;
+using Task_DirectoryTracker.Models.Entities;
+using Task_DirectoryTracker.Services;
+
+namespace Task_DirectoryTracker;
+
+/// <summary>
+/// This class contains extension methods for setting up services and configuring the application. <br /> 
+/// It is used to keep the Program.cs file clean and organized by separating the service registration and application configuration logic into a dedicated class.
+/// </summary>
+internal static class DependencyInjection
+{
+    /// <summary>
+    /// Registers services and configurations for the app
+    /// </summary>
+    internal static void AddServices(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        // Add services to the container
+        services.AddControllersWithViews();
+        services.AddSingleton<IHashService, HashService>();
+        services.AddScoped<IDirectoryScanner, DirectoryScanner>();
+        services.AddScoped<IChangeDetector, ChangeDetector>();
+        services.AddScoped<ISnapshotStorage, SnapshotStorage>();
+        services.AddScoped<IScanService, ScanService>();
+
+        // Configure addtional settings from appsettings.json
+        services.Configure<SnapshotSetting>(builder.Configuration.GetSection("SnapshotSettings"));
+    }
+
+    /// <summary>
+    /// Configures the application pipeline, including middleware and routing.
+    /// </summary>
+    /// <param name="app"> The WebApplication instance to configure </param>
+    /// <returns> The configured WebApplication instance </returns>
+    internal static WebApplication ConfigureApplication(this WebApplication app)
+    {
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseHsts();
+        }
+
+        app.UseMiddleware<ExceptionMiddleware>();
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+
+        app.MapStaticAssets();
+
+        app.MapControllerRoute(name: "default", pattern: "{controller=Scan}/{action=Index}/{id?}")
+            .WithStaticAssets();
+
+        app.Run();
+
+        return app;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,38 +1,7 @@
-using Task_DirectoryTracker.Abstractions;
-using Task_DirectoryTracker.Middlewares;
-using Task_DirectoryTracker.Models.Entities;
-using Task_DirectoryTracker.Services;
+using Task_DirectoryTracker;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddServices(builder);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddSingleton<IHashService, HashService>();
-builder.Services.AddScoped<IDirectoryScanner, DirectoryScanner>();
-builder.Services.AddScoped<IChangeDetector, ChangeDetector>();
-builder.Services.AddScoped<ISnapshotStorage, SnapshotStorage>();
-builder.Services.AddScoped<IScanService, ScanService>();
-
-// Configure addtional settings from appsettings.json
-builder.Services.Configure<SnapshotSetting>(builder.Configuration.GetSection("SnapshotSettings"));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseHsts();
-}
-
-app.UseMiddleware<ExceptionMiddleware>();
-
-app.UseHttpsRedirection();
-app.UseRouting();
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(name: "default", pattern: "{controller=Scan}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
-app.Run();
+WebApplication app = builder.Build();
+app.ConfigureApplication();
